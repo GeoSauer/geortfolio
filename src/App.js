5cc6fa122048ffb8c0fe393875be34414f7eb4cd
GeoSauer <geoffrey.sauer89@gmail.com>
2023-02-08 15:25:09 -0700

    add slight refactor after brain fart, NavBar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import Projects from './components/Projects/Projects';
+import Connect from './components/Connect/Connect';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/connect" component={Connect} />
+        <Route path="*" component={Home} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,19 +1,5 @@
-import { Route, Switch } from 'react-router-dom';
 import './About.css';
-import Home from '../Home/Home';
 
 export default function About() {
-  return (
-    <div>
-      About
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={Home} />
-        <Route exact path="/projects" component={Home} />
-        <Route exact path="/connect" component={Home} />
-        <Route exact path="/connect" component={Home} />
-        <Route path="*" component={Home} />
-      </Switch>
-    </div>
-  );
+  return <div>About</div>;
 }
