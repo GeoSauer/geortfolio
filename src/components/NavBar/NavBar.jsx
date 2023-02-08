0afb73e61cb798d0d40665a224faf22d3a0f0aad
GeoSauer <geoffrey.sauer89@gmail.com>
2023-02-08 16:12:07 -0700

    add NavBar links all working

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,26 @@
+import { Link, NavLink } from 'react-router-dom';
 import './NavBar.css';
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div>
+      <nav className="fancy-ternary-toggle">
+        <NavLink exact to="/">
+          Home
+        </NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/projects">Projects</NavLink>
+        <NavLink to="/connect">Connect</NavLink>
+        <Link to={{ pathname: 'https://github.com/geosauer' }} target="_blank">
+          GitHub
+        </Link>
+        <Link to={{ pathname: 'https://www.linkedin.com/in/geosauer' }} target="_blank">
+          LinkedIn
+        </Link>
+        <a href="" target="_blank">
+          Resume
+        </a>
+      </nav>
+    </div>
+  );
 }
