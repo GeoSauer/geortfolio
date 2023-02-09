2dc00739ae225941fe24cd8f7824865ea2b3c1a0
GeoSauer <geoffrey.sauer89@gmail.com>
2023-02-09 15:37:19 -0700

    add navbar icons for mobile browsing

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Oh hey it's my portfolio repo!
+
+<a href="https://www.flaticon.com/free-icons/profile" title="profile icons">Profile icons created by Prosymbols Premium - Flaticon</a>
+<a href="https://www.flaticon.com/free-icons/linkedin" title="linkedin icons">Linkedin icons created by Freepik - Flaticon</a>
+<a href="https://www.flaticon.com/free-icons/github" title="github icons">Github icons created by Pixel perfect - Flaticon</a>
+<a href="https://www.flaticon.com/free-icons/connection" title="connection icons">Connection icons created by Freepik - Flaticon</a>
+<a href="https://www.flaticon.com/free-icons/coding" title="coding icons">Coding icons created by edt.im - Flaticon</a>
+<a href="https://www.flaticon.com/free-icons/info" title="info icons">Info icons created by Freepik - Flaticon</a>
+<a href="https://www.flaticon.com/free-icons/home-button" title="home button icons">Home button icons created by Freepik - Flaticon</a>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ export default function NavBar() {
   const [navIsExpanded, setNavIsExpanded] = useState(false);
 
   return (
-    <div className="nav">
+    <div className="navbar">
       <button className="hamburger">
         <Hamburger
           size={30}
@@ -30,31 +30,31 @@ export default function NavBar() {
       <nav className={navIsExpanded ? 'nav-contents expanded' : 'nav-contents'}>
         <NavLink exact to="/">
           <span className="nav-text">Home</span>
-          <img src="" alt="Home" className="nav-img" />
+          <img src="/home.png" alt="Home" className="nav-img" />
         </NavLink>
         <NavLink to="/about">
           <span className="nav-text">About</span>
-          <img src="" alt="About" className="nav-img" />
+          <img src="/about.png" alt="About" className="nav-img" />
         </NavLink>
         <NavLink to="/projects">
           <span className="nav-text">Projects</span>
-          <img src="" alt="Projects" className="nav-img" />
+          <img src="/projects.png" alt="Projects" className="nav-img" />
         </NavLink>
         <NavLink to="/connect">
           <span className="nav-text">Connect</span>
-          <img src="" alt="Connect" className="nav-img" />
+          <img src="/connect.png" alt="Connect" className="nav-img" />
         </NavLink>
         <Link to={{ pathname: 'https://github.com/geosauer' }} target="_blank">
           <span className="nav-text">GitHub</span>
-          <img src="" alt="Github" className="nav-img" />
+          <img src="/github.png" alt="Github" className="nav-img" />
         </Link>
         <Link to={{ pathname: 'https://www.linkedin.com/in/geosauer' }} target="_blank">
           <span className="nav-text">LinkedIn</span>
-          <img src="" alt="LinkedIn" className="nav-img" />
+          <img src="/linkedin.png" alt="LinkedIn" className="nav-img" />
         </Link>
         <a href="" target="_blank">
           <span className="nav-text">Resume</span>
-          <img src="" alt="Resume" className="nav-img" />
+          <img src="/resume.png" alt="Resume" className="nav-img" />
         </a>
       </nav>
     </div>
